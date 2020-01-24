2504f522b7f168619986214b9b997c4d46e16c0d
PC <PC@192.168.1.54>
2020-01-24 07:22:37 +0100

    init

---
+++ HelloWorld/src/Main.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+	
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+		
+		
+		
+	}
+
+}
